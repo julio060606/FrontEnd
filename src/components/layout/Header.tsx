325c91b439298189d7460a5a747327db295cee92
Kevin Briceño Zegarra <165234699+KevinZegarra@users.noreply.github.com>
2026-09-15 17:39:25 -0500

    Fix de nombre FlightTracker a ChaskyFly en el header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -80,7 +80,7 @@ export const Header: React.FC<HeaderProps> = ({ onLoginClick }) => {
                   letterSpacing: '-0.02em',
                 }}
               >
-                FlightTracker
+                ChaskyFly
               </Typography>
             </Box>
 
